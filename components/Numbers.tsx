7bc6f5d52c371e1830369a0ad2837553c7a0ba7d
MIguel Eduardo Ovando Alvarez <miguel-workspace@outlook.com>
2024-04-19 19:40:09 -0600

    Update Numbers component

--- components/Numbers.tsx
+++ components/Numbers.tsx
@@ -1,7 +1,22 @@
-export default function Numbers () {
+export default function Numbers() {
   return (
-    <section>
-      numbs
-    </section>
+    <div className="flex flex-col md:flex-row lg:flex-col lg:items-end justify-center">
+      <h1 className="text-5xl font-bold text-center mb-10 lg:mb-40">
+        World Class <br /> Experts
+      </h1>
+      <div className="mb-20">
+        <h2 className="text-5xl font-medium text-center mb-4">+12K</h2>
+        <p className="text-slate-500 text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Lorem
+          ipsum dolor sit amet.
+        </p>
+      </div>
+      <div>
+        <h2 className='text-5xl font-medium mb-4 text-center'>0%</h2>
+        <p className='text-slate-500 text-center'>
+          Lorem ipsum dolor sit amet <br /> consectetur adipisicing elit.
+        </p>
+      </div>
+    </div>
   )
 }
